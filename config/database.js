35c4ee0db1b7beea310d0b0f147ff4dd78f5ff78
Navin Suryawanshi <108653526+suryawanshinavin@users.noreply.github.com>
2025-07-18 09:35:22 +0530

    config and setup

--- config/database.js
+++ config/database.js
@@ -1,15 +1,15 @@
-// config/database.js
-
 const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// Set up the MySQL connection using Sequelize
 const sequelize = new Sequelize(
-    "personal_finance_manager_nodejs",
-    "root",
-    "",
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: "localhost",
-        dialect: "mysql",
+        host: process.env.DB_HOST,
+        dialect: "mysql", // or 'postgres' if using PostgreSQL
+        logging: false,
     }
 );
 
